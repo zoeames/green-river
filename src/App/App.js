d7700216511546d52acfa3ee3d7be6984be908e8
Zoe Ames <zoeames@gmail.com>
2021-08-02 18:05:58 -0500

    setup changed

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>INSIDE APP COMPONENT</h2>
+      <button className='btn btn-info'>I am a button</button>
+    </div>
+  );
+}
+
+export default App;
